c8d2f8b31ca17da076f06630cda369b42a19b488
Ganji Nagendhra Prasad <gnagendhraprasad4@gmail.com>
2026-04-15 20:41:59 +0530

    Add files via upload

---
+++ CNN/OpenCV/edge_detection.py
@@ -0,0 +1,13 @@
+import  numpy as  np
+import cv2
+
+lena_img=cv2.imread('images/lena_color_512.tif')
+
+edge_img=cv2.Canny(lena_img,211,236)
+
+
+cv2.imshow('lena',lena_img)
+cv2.imshow('edge_img',edge_img)
+
+cv2.waitKey()
+cv2.destroyAllWindows()
